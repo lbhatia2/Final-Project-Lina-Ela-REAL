92a9a1a8e6ca5933f6750f1810d2a2dbe9dd0bd1
Lina Bhatia <lina@bhatia.org>
2022-08-01 09:10:04 -0500

    Initial Commit

---
+++ Final Project Lina Ela/Final_Project_Lina_ElaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_Lina_ElaApp.swift
+//  Final Project Lina Ela
+//
+//  Created by Lina Bhatia on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_Lina_ElaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
