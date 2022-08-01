1c6a3b4ed64af7cc931ad6c00f149bfb33e7156a
Ela Murat <ela.murat@icloud.com>
2022-08-01 11:36:27 -0500

    Changed text and title

---
+++ Final Project Lina Ela/SwiftUIViewAttractions.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewAttractions.swift
+//  Final Project Lina Ela
+//
+//  Created by Lina Bhatia on 8/1/22.
+//
+
+import SwiftUI
+
+struct SwiftUIViewAttractions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIViewAttractions_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewAttractions()
+    }
+}
---
+++ Final Project Lina Ela/SwiftUIViewFood.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewFood.swift
+//  Final Project Lina Ela
+//
+//  Created by Lina Bhatia on 8/1/22.
+//
+
+import SwiftUI
+
+struct SwiftUIViewFood: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIViewFood_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewFood()
+    }
+}
