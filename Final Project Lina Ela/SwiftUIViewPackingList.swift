3132db2dfab5033e2f40b2cbd3e0f053fbd7ee26
Lina Bhatia <lina@bhatia.org>
2022-08-01 14:18:06 -0500

    started checkbox list

--- Final Project Lina Ela/SwiftUIViewPackingList.swift
+++ Final Project Lina Ela/SwiftUIViewPackingList.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+struct checkListItem: Identifiable{
+    let id = UUID()
+    var name: String
+    var isChecked: Bool = false
+}
+
 struct SwiftUIViewPackingList: View {
+    @State var checklistItems = [checkListItem(name: "Pack", isChecked: false), checkListItem(name: "Do stuff", isChecked: false)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List{
+                ForEach(checklistItems) { checklistItems in
+                    HStack{
+                        Text(checklistItem.name)
+                        Spacer()
+                        Text(checklistItem.isChecked ? "✅" : "💭")
+                    }
+                    .onTapGesture {
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -18,3 +39,5 @@ struct SwiftUIViewPackingList_Previews: PreviewProvider {
         SwiftUIViewPackingList()
     }
 }
+
+
